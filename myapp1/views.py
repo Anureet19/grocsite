11dbcb88f72d1966a7ce2bae3e325b3913b7e0b9
Anureet Kaur <anureet@anureets-air.home>
2023-02-09 22:53:14 -0500

    Added a new view

--- myapp1/views.py
+++ myapp1/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 # Import necessary classes
+from django.http import HttpResponse
+from .models import Type, Item
 
 # Create your views here.
+def index(request):
+    type_list = Type.objects.all().order_by('id')
+    response = HttpResponse()
+    heading1 = '<p>' + 'Different Types: ' + '</p>'
+    response.write(heading1)
+    for type in type_list:
+        para = '<p>' + str(type.id) + ': ' + str(type) + '</p>'
+        response.write(para)
+    return response
